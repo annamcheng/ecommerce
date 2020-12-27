52d6c7a8b0968e5867226dda0c3bc53fe0e2746a
Anna Cheng <acheng0086@gmail.com>
2020-12-27 01:25:35 -0500

    Created VIEWS for all products on view page

--- store/views.py
+++ store/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
+# Create views here
 def store(request):
-	context = {}
+	# get all products
+	products = Product.objects.all()
+	# pass into context
+	context = {'products':products}
 	return render(request, 'store/store.html', context)
 
 def cart(request):
