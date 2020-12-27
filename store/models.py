994e1ae2e686e0efaea382029f2ad09f2714d8a7
Anna Cheng <acheng0086@gmail.com>
2020-12-26 22:04:16 -0500

    Added order, orderitem and product class in models

--- store/models.py
+++ store/models.py
@@ -13,3 +13,29 @@ class Customer(models.Model):
   # String Constructor passing in self as an argument
   def __str__(self):
     return self.name
+
+
+class Product(models.Model):
+  name = models.CharField(max_length=200, null=True)
+  price = models.FloatField()
+  digital = models.BooleanField(default=False, null=True, blank=True)
+
+  def __str__(self):
+    return self.name
+
+# Many to many relationship: Customer can have many orders
+# Orders belong to Customer
+class Order(models.Model):
+  customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+  date_ordered = models.DateTimeField(auto_now_add=True)
+  complete = models.BooleanField(default=False)
+  transaction_id = models.CharField(max_length=100, null=True)
+
+  def __str__(self):
+    return self.id
+
+class OrderItem(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	quantity = models.IntegerField(default=0, null=True, blank=True)
+	date_added = models.DateTimeField(auto_now_add=True)
