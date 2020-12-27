60edd8a3cc888f268bb95174da32c31b988850d7
Anna Cheng <acheng0086@gmail.com>
2020-12-27 02:20:08 -0500

    render image on main page & create Media URL and Media ROOT

--- store/models.py
+++ store/models.py
@@ -19,6 +19,7 @@ class Product(models.Model):
   name = models.CharField(max_length=200, null=True)
   price = models.FloatField()
   digital = models.BooleanField(default=False, null=True, blank=True)
+  image = models.ImageField(blank=True, null=True)
 
   def __str__(self):
     return self.name
