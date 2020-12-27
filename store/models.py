e6bf6134f140b188081791c574340c0567eb95b0
Anna Cheng <acheng0086@gmail.com>
2020-12-27 00:13:34 -0500

    Migrated models

--- store/models.py
+++ store/models.py
@@ -26,16 +26,30 @@ class Product(models.Model):
 # Many to many relationship: Customer can have many orders
 # Orders belong to Customer
 class Order(models.Model):
-  customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+  customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
   date_ordered = models.DateTimeField(auto_now_add=True)
-  complete = models.BooleanField(default=False)
+  complete = models.BooleanField(default=False, blank=False, null=True)
   transaction_id = models.CharField(max_length=100, null=True)
 
   def __str__(self):
-    return self.id
+    return str(self.id)
 
+# Many to one relationship: cart has many items, items belong to cart
 class OrderItem(models.Model):
-	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
-	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
 	quantity = models.IntegerField(default=0, null=True, blank=True)
 	date_added = models.DateTimeField(auto_now_add=True)
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address= models.CharField(max_length=200, null=True)
+    city = models.CharField(max_length=200, null=True)
+    state = models.CharField(max_length=200, null=True)
+    zipcode = models.CharField(max_length=200, null=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+      return self.address
