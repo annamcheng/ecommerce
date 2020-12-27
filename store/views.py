7f4598c796a21cf922ef1290f53d210a1d54bec8
Anna Cheng <acheng0086@gmail.com>
2020-12-27 02:40:50 -0500

    Connect cart items to Customer's order. Create for in loop on cart page

--- store/views.py
+++ store/views.py
@@ -10,7 +10,17 @@ def store(request):
 	return render(request, 'store/store.html', context)
 
 def cart(request):
-	context = {}
+	if request.user.is_authenticated:
+		# connects user to customer
+		customer = request.user.customer
+		# connects customer's order
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		# get items attached to order
+		items = order.orderitem_set.all()
+	else:
+		# return empty value if customer is not logged in/authenticated
+		items = []
+	context = {'items':items}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
