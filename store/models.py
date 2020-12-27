05a42ea5c630aa9f4dd27839b01da00d01fc716c
Anna Cheng <acheng0086@gmail.com>
2020-12-26 21:57:50 -0500

    Created Customer class in models

--- store/models.py
+++ store/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+# Inherit models from Model for Customer class
+# User can only have one Customer
+# Customer can only have one User
+class Customer(models.Model):
+  user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+  name = models.CharField(max_length=200, null=True)
+  email = models.CharField(max_length=200, null=True)
+
+  # String Constructor passing in self as an argument
+  def __str__(self):
+    return self.name
