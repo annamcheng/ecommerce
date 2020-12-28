862a1e351e656ffd90dccae98d706aa639673274
Anna Cheng <acheng0086@gmail.com>
2020-12-27 22:07:09 -0500

    added productId to the backend and created updateUserOrder method

--- static/js/cart.js
+++ static/js/cart.js
@@ -15,7 +15,23 @@ for (var i = 0; i < updateBtns.length; i++) {
     if (user == "AnonymousUser") {
       console.log("User is not authenticated");
     } else {
-      console.log("User is authenticated, sending data...");
+        updateUserOrder(productId, action);
     }
   });
+}
+
+const updateUserOrder = (productId, action) =>{
+  console.log('User is authenticated, sending data...')
+  var url = '/update_item/'
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+    body.JSON.stringify({'productId':productId, 'action':action})
+  }).then((response) =>{
+    return response.json()
+  }).then((data) =>{
+    console.log('Data', data)
+  })
 }
--- store/views.py
+++ store/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import *
 
 # Create views here
@@ -40,3 +41,6 @@ def checkout(request):
 		
 	context = {'items':items, 'order':order}
 	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	return JsonResponse('Item was added', safe=False)
