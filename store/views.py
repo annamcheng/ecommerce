3f52a1b7439f4f15d6c58d3f0b221472f7aabce6
Anna Cheng <acheng0086@gmail.com>
2020-12-29 01:11:27 -0500

    product id, name, price and image are accessible  on cart page

--- store/views.py
+++ store/views.py
@@ -33,11 +33,39 @@ def cart(request):
 		items = order.orderitem_set.all()
 		cartItems = order.get_cart_items
 	else:
+		try: 
+			cart = json.loads(request.COOKIES['cart'])
+		except:
+			cart = {}
+		print('Cart:', cart)
 		# return empty value if customer is not logged in/authenticated
 		items = []
 		order = {'get_cart_total':0, 'get_cart_items':0, 'shipping':False}
 		cartItems = order['get_cart_items']
-		
+
+		# update cart number on cart page
+		for i in cart:
+			cartItems += cart[i]['quantity']
+			# accessing product and price on cart page
+			product = Product.objects.get(id=i)
+			total = (product.price * cart[i]['quantity'])
+			# accessing order total and items on cart page
+			order['get_cart_total'] += total
+			order['get_cart_items'] += cart[i]['quantity']
+			# accesses item list on cart page
+			item = {
+				'product': {
+					'id': product.id,
+					'name': product.name,
+					'price': product.price,
+					'imageURL':product.imageURL
+				},
+				'quantity': cart[i]['quantity'],
+				'get_total':total,
+			}
+			items.append(item)
+
+		# elements passed into context dictionary
 	context = {'items':items, 'order':order, 'cartItems':cartItems}
 	return render(request, 'store/cart.html', context)
 
