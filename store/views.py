90eb07121fcd1c739bbbfc1da74cdd6befaa3bcf
Anna Cheng <acheng0086@gmail.com>
2020-12-29 15:55:06 -0500

    deleted django framework

--- store/views.py
+++ store/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-from rest_framework import viewsets
-from rest_framework import permissions
 import json
 import datetime
 
