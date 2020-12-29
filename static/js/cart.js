0034ee63731ca188742f022a1f04aefdbbfd2c89
Anna Cheng <acheng0086@gmail.com>
2020-12-29 00:54:15 -0500

    updated browsers cookies for users that are not authenticated

--- static/js/cart.js
+++ static/js/cart.js
@@ -6,23 +6,24 @@ var updateBtns = document.getElementsByClassName("update-cart");
 //for loop to loop through each button
 for (i = 0; i < updateBtns.length; i++) {
   // add eventlistener on click event
-  updateBtns[i].addEventListener("click", function () {
+  updateBtns[i].addEventListener("click", function() {
     var productId = this.dataset.product;
     var action = this.dataset.action;
     console.log("productId:", productId, "Action:", action);
     console.log("USER:", user);
 
-    if (user == "AnonymousUser") {
-      addCookieItem(productId, action);
+    if (user == "AnonymousUser"){
+      console.log("Not logged in")
+      addCookieItem(productId, action)
     } else {
-      updateUserOrder(productId, action);
+      updateUserOrder(productId, action)
     }
-  });
+  })
 }
 
 // send POST data from backend to Django
 function updateUserOrder(productId, action) {
-  console.log("User is authenticated, sending data...");
+  console.log("User is authenticated, sending data...")
 
   var url = "/update_item/";
 
@@ -32,7 +33,7 @@ function updateUserOrder(productId, action) {
       "Content-Type": "application/json",
       "X-CSRFToken": csrftoken,
     },
-    body: JSON.stringify({ productId: productId, action: action }),
+    body: JSON.stringify({'productId': productId, 'action': action}),
   })
     .then((response) => {
       return response.json();
@@ -62,7 +63,6 @@ function addCookieItem(productId, action) {
     }
   }
   console.log("CART:", cart);
-  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/";
-
+  document.cookie = "cart=" + JSON.stringify(cart) + ";domain=;path=/"
   location.reload();
 }
